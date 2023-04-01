eb7826b20a379eb18dca9f1b1b9e455f1f9e20e0
Maria <byankina_masha@mail.ru>
2023-04-01 18:44:49 +0700

    Homework for lesson 4

---
+++ Lesson4/Lesson 4/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class Program
+{
+    delegate void Message(int x);
+    public static void Main ()
+    {
+       Counter counter = new Counter();
+       Handler1 handler1 = new Handler1();
+       Handler2 handler2 = new Handler2();
+
+        counter.AlarmNum += handler1.Message;
+        counter.AlarmNum += handler2.Message;
+
+        counter.Count();
+    }
+
+   
+
+}
